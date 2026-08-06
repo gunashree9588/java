d0e0547fddf3018a3becf7b4c9d86a21a666b3f2
gunashree9588 <gunashree9581@gmail.com>
2026-08-06 23:59:17 +0530

    Update throws.java

--- blue.java
+++ blue.java
@@ -1,12 +1,12 @@
 import java.io.IOException;         //throws in String
 public class blue { 
-    static void read()throws IOException{
-            throw new IOException("file not found");
+    static void read()throws IOException{               //throws declaration
+            throw new IOException("file not found");     //throw statement
 
     } 
     public static void main(String[] args)throws IOException{
               read();
-            
+            //output : file not found
     
         }  
     }
